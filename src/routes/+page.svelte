ddb834613e05a3b7e898a3d909a8bfbccc42de7e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-27 15:02:56 +0200

    fix: :art: import constants from module

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,131 +1,21 @@
 <script>
-	import { afterUpdate } from "svelte";
-	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
+	import { KEYBOARD, MODIFIER_KEYS, OTHER_KEYS } from "$lib/constants.js";
 
 	export let data;
 	Error.stackTraceLimit = 0;
 	// Error.stackTraceLimit = Infinity;
 
 	let dvorak = true;
+	let pressedKey = {};
 
 	$: layout = dvorak ? "dvorak" : "qwerty";
-
-	const keyboard = {
-		dvorak: {
-			row1: [
-				"esc",
-				"empty",
-				"f1",
-				"f2",
-				"f3",
-				"f4",
-				"empty",
-				"f5",
-				"f6",
-				"f7",
-				"f8",
-				"empty",
-				"f9",
-				"f10",
-				"f11",
-				"f12",
-			],
-			row2: ["§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "´", "backspace"],
-			row3: ["tab", ":", ";", "=", "p", "y", "f", "g", "c", "r", "l", ",", "¨"],
-			row4: ["caps", "a", "o", "e", "u", "i", "d", "h", "t", "n", "s", "-", "'", "enter"],
-			row5: [
-				"shift",
-				"<",
-				".",
-				"q",
-				"j",
-				"k",
-				"x",
-				"b",
-				"m",
-				"w",
-				"v",
-				"z",
-				"shift2",
-				"empty",
-				"up",
-			],
-			row6: [
-				"control",
-				"meta",
-				"alt",
-				" ",
-				"altgr",
-				"meta",
-				"fn",
-				"control",
-				"left",
-				"down",
-				"right",
-			],
-		},
-		qwerty: {
-			row1: [
-				"esc",
-				"empty",
-				"f1",
-				"f2",
-				"f3",
-				"f4",
-				"empty",
-				"f5",
-				"f6",
-				"f7",
-				"f8",
-				"empty",
-				"f9",
-				"f10",
-				"f11",
-				"f12",
-			],
-			row2: ["§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "´", "backspace"],
-			row3: ["tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "å", "¨"],
-			row4: ["caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä", "'", "enter"],
-			row5: [
-				"shift",
-				"<",
-				"z",
-				"x",
-				"c",
-				"v",
-				"b",
-				"b",
-				"m",
-				",",
-				".",
-				"-",
-				"shift2",
-				"empty",
-				"up",
-			],
-			row6: [
-				"control",
-				"meta",
-				"alt",
-				" ",
-				"altgr",
-				"meta",
-				"fn",
-				"control",
-				"left",
-				"down",
-				"right",
-			],
-		},
-	};
 
 	function getKeyData(key) {
 		explanations = data.json.filter((a) => a.key === key);
 		return keys;
 	}
 
-	let pressedKey = {};
 	function handleKeydown(e) {
 		pressedKey[e.key.toLowerCase()] = e.key.toLowerCase();
 
@@ -154,6 +44,7 @@
 
 		console.log(e);
 	}
+
 	function handleKeyup(e) {
 		pressedKey = {};
 		//  console.log(e)
@@ -161,79 +52,13 @@
 
 	function getUnboundKeys() {
 		// thanks chatgpt (... took 10 tries though)
-		const modifierKeys = ["ctrl", "shift", "alt"];
-		const otherKeys = [
-			"a",
-			"b",
-			"c",
-			"d",
-			"e",
-			"f",
-			"g",
-			"h",
-			"i",
-			"j",
-			"k",
-			"l",
-			"m",
-			"n",
-			"o",
-			"p",
-			"q",
-			"r",
-			"s",
-			"t",
-			"u",
-			"v",
-			"w",
-			"x",
-			"y",
-			"z",
-			"0",
-			"1",
-			"2",
-			"3",
-			"4",
-			"5",
-			"6",
-			"7",
-			"8",
-			"9",
-			"space",
-			"enter",
-			"tab",
-			"escape",
-			"backspace",
-			"capslock",
-			"up",
-			"down",
-			"left",
-			"right",
-			"oem_plus",
-			"oem_minus",
-			// laptop keys only
-			// 'home', 'end', 'insert', 'delete', 'pageup', 'pagedown',
-			// 'numlock', 'scrolllock', 'pause', 'printscreen',
-			"f1",
-			"f2",
-			"f3",
-			"f4",
-			"f5",
-			"f6",
-			"f7",
-			"f8",
-			"f9",
-			"f10",
-			"f11",
-			"f12",
-			// Add more keys as needed
-		];
+
 		function generateCombinations(modifiers, keys) {
 			const combinations = [];
 
 			// Sort modifier keys based on desired order
 			modifiers.sort((a, b) => {
-				return modifierKeys.indexOf(a) - modifierKeys.indexOf(b);
+				return MODIFIER_KEYS.indexOf(a) - MODIFIER_KEYS.indexOf(b);
 			});
 
 			// Generate combinations
@@ -249,11 +74,10 @@
 					);
 				}
 			}
-
 			return combinations;
 		}
 
-		let allCombinations = generateCombinations(modifierKeys, otherKeys);
+		let allCombinations = generateCombinations(MODIFIER_KEYS, OTHER_KEYS);
 		allCombinations = allCombinations.filter((key) => !/^[a-z]$|^shift\+[a-z]$|^[0-9]$/.test(key));
 		// console.log(allCombinations);
 		// console.log(data.json);
@@ -270,8 +94,6 @@
 	let explanations;
 </script>
 
-<div />
-
 <svelte:window on:keydown|preventDefault={handleKeydown} on:keyup|preventDefault={handleKeyup} />
 <div class="flex flex-col items-center">
 	<h1 class="text-4xl">VSCode hotkey viewer</h1>
@@ -285,7 +107,7 @@
 </div>
 <main class="ml-8 flex h-screen w-[screen-100px]">
 	<div class="h-[400px] w-[1000px] flex-col rounded bg-slate-800 p-1 shadow-lg shadow-black/50">
-		{#each Object.values(keyboard[layout]) as row}
+		{#each Object.values(KEYBOARD[layout]) as row}
 			<div>
 				{#each row as key}
 					<button
